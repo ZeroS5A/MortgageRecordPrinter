925c819a5900aadadd165f2cbc90484bde78551b
ZeroS <work_pc>
2026-03-19 16:04:01 +0800

    V260319-将VBS改为bat启动

---
+++ 🚀 启动系统.bat
@@ -0,0 +1,19 @@
+﻿@echo off
+:: ==========================================
+:: 隐藏 CMD 窗口逻辑 (核心修改)
+:: ==========================================
+if "%1" == "h" goto begin
+:: 利用 mshta 调用 vbscript 隐藏运行自身
+mshta vbscript:createobject("wscript.shell").run("""%~f0"" h",0)(window.close)&&exit
+:begin
+
+setlocal
+
+:: ==========================================
+:: 基础配置
+:: ==========================================
+
+@echo off
+chcp 65001 >nul
+set PYTHONUTF8=1
+streamlit run app.py --server.port 8502
--- 🚀 启动系统.vbs
+++
@@ -1,4 +0,0 @@
-Set ws = CreateObject("WScript.Shell")
-' 0 表示隐藏命令行黑框，False 表示不阻塞直接返回
-' 这里也加入了编码设置，确保后台静默运行时也能正确处理中文字符
-ws.Run "cmd /c chcp 65001 & set PYTHONUTF8=1 & streamlit run app.py", 0, False
